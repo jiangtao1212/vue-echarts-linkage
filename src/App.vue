022d3975ce4cade4e1c7897793584f4ca831bbea
jiangtao <1106950092@qq.com>
2024-09-03 17:18:28 +0800

    新版本0.0.2-beta.1测试

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import { ref } from "vue";
 import { RouterLink, RouterView } from 'vue-router';
 import EchartsLinkageView from "@/views/echarts-linkage-view.vue";
 import Demo1 from "@/demo/demo1.vue";
+import Demo2 from "@/demo/demo2.vue";
 </script>
 
 <template>
-  <EchartsLinkageView />
+  <!-- <EchartsLinkageView /> -->
   <!-- <Demo1 /> -->
+  <Demo2 />
 </template>
 
 <style scoped></style>
